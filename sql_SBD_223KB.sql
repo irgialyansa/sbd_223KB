806eaba318563e785478133c61083aa4cc114cc3
irgialyansa <irgialyansa@gmail.com>
2024-06-13 02:55:59 +0700

    add new stat

--- sql_SBD_223KB.sql
+++ sql_SBD_223KB.sql
@@ -5,4 +5,27 @@ create table barang(
 	barangStock INT NOT NULL,
 	barangDesc VARCHAR(200),
 	PRIMARY KEY ( barangID )
-);
+); 
+
+drop table barang; 
+
+insert into barang 
+(barangName, barangHarga, barangStock, barangDesc)
+values
+("bolu", 10000, 10, "bolu susu"),
+("roti", 5000, 5, "roti keju"),
+("pukis", 6000, 3, "pukis kismis");
+
+
+select * from barang
+
+select * from barang 
+where barangStock > 9;
+
+select barangName , barangHarga , barangDesc from barang
+
+select b.barangName , b.barangHarga  from barang b
+where barangName = "roti"
+
+update barang set barangName = "bolu gulung"
+where barangStock = 20
